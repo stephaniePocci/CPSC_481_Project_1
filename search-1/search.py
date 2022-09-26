e33d9a5c383cc9992c02d253d6dac34b7b179596
karnikaavelumani <60043611+karnikaavelumani@users.noreply.github.com>
2022-09-25 19:43:02 -0700

    breadthFirstSearch

--- search-1/search.py
+++ search-1/search.py
@@ -108,6 +108,39 @@ def depthFirstSearch(problem):
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
     "*** YOUR CODE HERE ***"
+        #to be explored (FIFO)
+    frontier = util.Queue()
+    
+    #previously expanded states (for cycle checking), holds states
+    exploredNodes = []
+    
+    startState = problem.getStartState()
+    startNode = (startState, [], 0) #(state, action, cost)
+    
+    frontier.push(startNode)
+    
+    while not frontier.isEmpty():
+        #begin exploring first (earliest-pushed) node on frontier
+        currentState, actions, currentCost = frontier.pop()
+        
+        if currentState not in exploredNodes:
+            #put popped node state into explored list
+            exploredNodes.append(currentState)
+
+            if problem.isGoalState(currentState):
+                return actions
+            else:
+                #list of (successor, action, stepCost)
+                successors = problem.getSuccessors(currentState)
+                
+                for succState, succAction, succCost in successors:
+                    newAction = actions + [succAction]
+                    newCost = currentCost + succCost
+                    newNode = (succState, newAction, newCost)
+
+                    frontier.push(newNode)
+
+    return actions
     util.raiseNotDefined()
 
 def uniformCostSearch(problem):
